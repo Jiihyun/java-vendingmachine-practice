12ef24b223a4de43ce23713a7bd6fcd64736d52d
Jiihyun <cwlgusc@gmail.com>
2023-12-01 23:06:03 +0900

    feat(VendingMachine): 투입 금액 검증 추가

---
+++ src/main/java/vendingmachine/domain/ProductInfos.java
@@ -0,0 +1,11 @@
+package vendingmachine.domain;
+
+import java.util.List;
+
+public class ProductInfos {
+    private final List<ProductInfo> productInfos;
+
+    public ProductInfos(List<ProductInfo> productInfos) {
+        this.productInfos = productInfos;
+    }
+}
